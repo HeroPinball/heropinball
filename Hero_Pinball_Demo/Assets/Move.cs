029381835d1bdbe7f1b89d6dba547df47686314e
HunterOka <okah1@nku.edu>
2014-10-28 01:44:40 -0400

    Upload demo

---
+++ Hero_Pinball_Demo/Assets/Move.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour 
+{
+
+	private bool facingLeft = true;
+	private bool canJump = true;
+	public float torque = 1;
+	public int jumpStrength = 50;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Input.GetMouseButton (0)) {
+			animator.Play("SwordSwing");
+				}
+
+		if (Input.GetAxis ("Horizontal") > 0 && rigidbody2D.velocity.x < 10) 
+		{
+			if (facingLeft)
+				Flip();
+			rigidbody2D.AddForce (Vector2.right * 25);
+			//rigidbody2D.AddTorque(-torque);
+			rigidbody2D.angularDrag = 8;
+		}
+		
+		else if (Input.GetAxis ("Horizontal") < 0 && rigidbody2D.velocity.x > -10) 
+		{
+			if (!facingLeft)
+				Flip();
+
+			rigidbody2D.AddForce (Vector2.right * -25);
+			//rigidbody2D.AddTorque(torque);
+			rigidbody2D.angularDrag = 8;
+		}
+
+		else
+		{
+			rigidbody2D.angularDrag = 1;
+		}
+
+		//jump
+		if (Input.GetAxis ("Vertical") > 0 && canJump) 
+		{
+
+			rigidbody2D.AddForce (Vector2.up * jumpStrength);
+			canJump = false;
+		}
+
+		//If goes off bottom of screen, return to top
+		if (transform.position.y < -20) {
+						transform.position = new Vector3 (0, 11, 0);
+			rigidbody2D.AddForce (Vector2.up * 1000);//Counteract gravity
+				}
+
+		//rotate upright
+		//if (rigidbody2D.angularVelocity > 0) 
+		{
+			//rigidbody2D.angularVelocity = 0;
+		}
+	}
+
+	void Flip()
+	{
+		transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+		facingLeft = !facingLeft;
+	}
+
+	void OnCollisionEnter()
+	{
+		Debug.Log ("collide");
+		canJump = true;
+	}
+}
---
+++ Hero_Pinball_Demo/Assets/reset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionrEnter2D()
+	{
+		Debug.Log ("hitResetter");
+		//other.transform.position = (new Vector3 (0, 0, 0));
+	}
+}
