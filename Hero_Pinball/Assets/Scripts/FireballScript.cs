332b3ff452077ff7d9f8705a13365586dc4cb300
ONLYUSEmeFEET <onlyusemefeet@gmail.com>
2014-11-11 16:27:25 -0500

    boss/pickup implementations

---
+++ Hero_Pinball/Assets/Scripts/FireballScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour
+{
+
+		public Transform fireball;
+		public float numFireballs;
+		public float maxFireballs = 3;
+		public static float fireSpawn = 10;
+		public float firetimer;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				firetimer -= Time.deltaTime;
+				if (firetimer < 0 && firetimer > -0.1 && numFireballs < 3) {
+						Instantiate (fireball);
+						firetimer = fireSpawn;
+						numFireballs++;
+						print ("You're in the loop!");
+				} else {
+						print ("Not in the loop.");
+				}
+		}
+}       
