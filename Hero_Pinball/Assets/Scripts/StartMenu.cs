6d071efd27d123a76ece8d8160c49bd85900b93d
HunterOka <okah1@nku.edu>
2014-11-14 13:44:56 -0500

    Start Menu

---
+++ Hero_Pinball/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿
+//Attach to main camera
+using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour 
+{
+
+	public GUISkin custGUISkin;
+
+
+	void OnGUI()
+	{
+
+		GUI.skin = custGUISkin;
+		//Displays buttons
+		if (GUI.Button (new Rect (Screen.width * 0.18f, Screen.height * 0.18f, 270, 85), "Play")) 
+		{
+			Application.LoadLevel(1);
+		}
+
+		GUI.Button (new Rect (Screen.width * 0.18f, Screen.height * 0.25f, 270, 85), "Instructions");
+
+		GUI.Button (new Rect (Screen.width * 0.18f, Screen.height * 0.32f,270, 85), "High Scores");
+
+	}
+
+	
+
+}
